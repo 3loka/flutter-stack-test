d022c98d4504b9e8a1352b35dbc6277a60aa2566
Trilok Ramakrishna <84232154+3loka@users.noreply.github.com>
2021-10-03 00:54:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
+    // final wordPair = WordPair.random();
+    return const MaterialApp(
       title: 'Startup name generator',
       home: RandomWords(),
     );
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
